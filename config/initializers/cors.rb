a9b2d485deb2c7e39ac544d7ae745e3386d844e9
krestko <Katherine.V.Restko@gmail.com>
2018-12-22 15:02:52 -0600

    routes changed to local machine for modificiation

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://baby-names-frontend.herokuapp.com'
+    origins 'http://localhost:3000'
+    # 'https://baby-names-frontend.herokuapp.com'
 
     resource '*',
       headers: :any,
